077df4e2e06a69e50a66b9c5d198179ba6fe4070
karl078 <carloliuzza@gmail.com>
2025-03-16 08:17:10 +0100

    Aggiornamento misurazione acqua del 16-3-2025-8:17

--- readFileAndGraph4.py
+++ readFileAndGraph4.py
@@ -1,166 +1,141 @@
 #!/usr/bin/env python3
 
-# date: 2019.08.04
-# https://stackoverflow.com/questions/57341566/my-bar-chart-does-not-appear-on-screen-how-can-i-fix-that
-#
-# https://mpld3.github.io/index.html
-#
-
-#from flask import Flask
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
 import mpld3
 import numpy as np
-import ftplib
 import time
 import datetime
 import calendar
 from mese import mese
 from git import Repo
-#app = Flask(__name__)
 
-#@app.route('/')
-PATH_OF_GIT_REPO = '/home/pi/python-scripts/.git'  # make sure .git folder is properly configured
+# Global Configuration (move these to a config file later)
+PATH_OF_GIT_REPO = '/home/pi/python-scripts/.git'
 
-righe = [] #definisco il vettore per accumulare le righe del file
-righeJ = [] #definisco il vettore per accumulare le righe del file dell'acqua comunale
-date = []
-giorniMese = []
-giorniMeseJ = []
-giorniMeseInt = []
-giorniMeseIntJ = []
-temp = []
-ore = []
-altezze = []
-altezzeJ = []
-
-#FUNZIONE per aggiornare la pagina html statica su Github Pages
-# source tutorials:
-#  https://blog.balasundar.com/automate-git-operations-using-python
-#  https://stackoverflow.com/questions/41836988/git-push-via-gitpython
-# FIX errore credenziali: https://levelup.gitconnected.com/fix-password-authentication-github-3395e579ce74
-# aggiornare token ogni 90 giorni https://techglimpse.com/git-push-github-token-based-passwordless/
-# rigenerare il token (se necessario, anche se ad oggi ho scelto quello never-expire, e inviare da riga di comando quanto sotto:
-# RICORDARSI di selezionare l'opzione REPO quando si rigenera il token. fonte: https://github.com/orgs/community/discussions/23475
-# RICORDARSI di dare il comando sotto riportato nella cartella python-scripts
-# git remote set-url origin https://<GITHUB_ACCESS_TOKEN>@github.com/<GITHUB_USERNAME>/<REPOSITORY_NAME>.git
-# git remote set-url origin https://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@github.com/karl078/ruggero.git
-
+# Function to push to GitHub Pages
 def git_push():
     try:
         repo = Repo(PATH_OF_GIT_REPO)
         repo.git.add(update=True)
         repo.index.add(['/home/pi/python-scripts/index.html'])
-        repo.index.commit('Aggiornamento misurazione acqua del ' + str(now.day) + '-' + str(now.month) + '-' + str(now.year))
+        repo.index.commit('Aggiornamento misurazione acqua del ' + str(now.day) + '-' + str(now.month) + '-' + str(now.year)+'-'+str(now.hour)+':'+str(now.minute))
         origin = repo.remote(name='origin')
         origin.push()
         print("File caricato su GITHUB PAGES!\n")
-    except:
-        print('Some error occured while pushing the code')
+    except Exception as e:
+        print(f'Some error occurred while pushing the code: {e}')
 
-# funzione per aggiungere il valore misurazioni sul grafico
-def addlabels(x,y):
+# Function to add labels to the bars
+def addlabels(ax, x, y, width):
+    """Adds value labels above each bar in the chart.
+
+    Args:
+        ax: the axis where labels will be added.
+        x: A list of x-coordinates of the bars.
+        y: A list of bar heights (values).
+        width: width of each bar.
+    """
     for i in range(len(x)):
-        plt.text(x[i]+0.25,y[i]+2,y[i], color='red', fontsize=14, rotation=90)
+        # Center the label horizontally above the bar
+        ax.text(x[i] + width / 2,     # Posizione orizzontale centrata (lasciata come prima)
+                y[i] + 50,            # Offset maggiore sull'asse Y
+                f"{y[i]:.0f}" if y[i] == int(y[i]) else f"{y[i]:.1f}",
+                color='#FF0000',      # Rosso brillante
+                fontsize=20,          # Dimensione font aumentata
+                fontweight='bold', 
+                ha='center',          # Centrato orizzontalmente (come prima)
+                va='bottom',          # Torniamo a 'bottom' come nella versione originale
+                rotation=90,          # Mantiene la rotazione originale
+                path_effects=[path_effects.withStroke(linewidth=5, foreground='white')])
+                        
 
+# Function to read and parse a log file
+def read_and_parse_log_file(log_file_path):
+    """Reads a log file, parses the data, and returns days and values."""
+    righe = []
+    giorniMeseInt = []
+    altezze = []
 
+    try:
+        with open(log_file_path, 'r') as fo:
+            for rec in fo:
+                righe.append(rec)
+    except FileNotFoundError:
+        print(f"Error: Log file not found at {log_file_path}")
+        return [], []  # Return empty lists on error
+
+    for i in righe:
+        try:
+            temp = i.split()
+            giorniMese = temp[0]
+            giorniMeseInt.append(int(giorniMese[0:2]))
+            altezze.append(float(temp[2]))
+        except (ValueError, IndexError):
+            print(f"Warning: Invalid data format in log file: {i}")
+            # You could choose to handle this differently, e.g., log the error, skip the line, etc.
+
+    return giorniMeseInt, altezze
+
+# Get current date and month
 dataOggi = time.strftime("%d/%m/%Y")
-meseOggi=dataOggi[3:5]
+meseOggi = dataOggi[3:5]
 print("Ricavo mese in lettere..\n")
-meseNum=int(meseOggi)
-#meseNum=4 #per forzare il mese
-meseStr=mese(meseNum)
+meseNum = int(meseOggi)
+meseStr = mese(meseNum)
 print(meseStr)
 
 print("Ricavo numero di giorni del mese..\n")
 now = datetime.datetime.now()
 numGiorni = calendar.monthrange(now.year, now.month)[1]
-#numGiorni=31 #per forzare i giorni
 print(numGiorni)
 
+# Read data for well water and municipal water
+giorniMeseInt, altezze = read_and_parse_log_file('/home/pi/python-scripts/acqua.log')
+giorniMeseIntJ, altezzeJ = read_and_parse_log_file('/home/pi/python-scripts/acquaComune.log')
 
-######## LEGGO DATI CISTERNA POZZO ##########
-#accumulo i dati delle righe del file
-with open('/home/pi/python-scripts/acqua.log') as fo:
-    for rec in fo:
-        righe.append(rec)
+# Create the plots
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 7))  # Create figure and axes (increase size)
+fig.tight_layout(pad=5.0) #add more space between subplots
 
-# splitto i dati ricavando le informazioni per il grafico:
-# giorni nelle ascisse
-# altezze nelle ordinate
-for i in righe:
-    temp=i.split()
-    giorniMese=temp[0]
-    giorniMeseInt.append(int(giorniMese[0:2])) #verificare se necessaria questa forzatura di conversione in INT
-    altezze.append(float(temp[2]))
-#    date.append(i[0:8])
-#print(giorniMeseInt)
+# Chart 1 (Well Water)
+ax1.bar(giorniMeseInt, altezze, width=0.6, color='#66b3ff', edgecolor='#003366')  # Use a better color and border
+addlabels(ax1, giorniMeseInt, altezze, 0.6) #change width
+ax1.yaxis.grid(True, color='lightgray', linestyle='--')  # Lighter gridlines
+ax1.set_ylabel("ALTEZZA [cm]", fontsize=14) #reduce font size
+ax1.set_ylim([0, 425])
+ax1.set_xlim([0, numGiorni + 0.5])
+ax1.set_xticks(np.arange(1, numGiorni + 1, 1.0))
+ax1.tick_params(axis='both', which='major', labelsize=12)  # Increase tick label size #reduce font size
+ax1.set_title("Cisterna Pozzo - " + meseStr + " " + str(now.year), fontsize=16)  # Title for Well Water subplot (with month)
 
-######## LEGGO DATI CISTERNA COMUNALE ##########
-with open('/home/pi/python-scripts/acquaComune.log') as foJ:
-    for recJ in foJ:
-        righeJ.append(recJ)
-#print(righeJ)
+# Chart 2 (Municipal Water)
+ax2.bar(giorniMeseIntJ, altezzeJ, width=0.6, color='#90ee90', edgecolor='#006400')  # Use a different color and border
+addlabels(ax2, giorniMeseIntJ, altezzeJ, 0.6) #change width
+ax2.yaxis.grid(True, color='lightgray', linestyle='--')  # Lighter gridlines
+ax2.set_xlabel("GIORNO", fontsize=14) #reduce font size
+ax2.set_ylabel("ALTEZZA [cm]", fontsize=14) #reduce font size
+ax2.set_ylim([0, 425])
+ax2.set_xlim([0, numGiorni + 0.5])
+ax2.set_xticks(np.arange(1, numGiorni + 1, 1.0))
+ax2.tick_params(axis='both', which='major', labelsize=12)  # Increase tick label size #reduce font size
+ax2.set_title("Cisterna Comunale - " + meseStr + " " + str(now.year), fontsize=16)  # Title for Municipal Water subplot (with month)
 
-# splitto i dati ricavando le informazioni per il grafico:
-# giorni nelle ascisse
-# altezze nelle ordinate
-for j in righeJ:
-    temp=j.split()
-    giorniMeseJ=temp[0]
-    giorniMeseIntJ.append(int(giorniMeseJ[0:2])) 
-    altezzeJ.append(float(temp[2]))
-#    date.append(i[0:8])
-#print([min(giorniMeseIntJ),max(giorniMeseIntJ)])
-
-#plots = plt.bar(giorniMeseInt, altezze, width=0.5) #creo il grafico
-#fig = plt.figure()
-fig, ax = plt.subplots()
-
-#PRIMO SUBPLOT
-plt.subplot(2, 1, 1)    #GRAFICO ACQUA POZZO
-plt.bar(giorniMeseInt, altezze, width=0.5)
-addlabels(giorniMeseInt, altezze)
-#for p in pps:
-#    height = p.get_height()
-#    ax.annotate('{}'.format(height), xy=p.get_x() + p.get_width() / 2, height), xytext=(0, 3), textcoords="offset points", ha='center', va='bottom')
-
-axes = plt.gca()
-axes.yaxis.grid()   #assi orizzontali
-#plt.xlabel("GIORNO")
-plt.ylabel("ALTEZZA [cm]")
+# Common Title
 TITOLO = meseStr + ' ' + str(now.year) + ": SOPRA cisterna pozzo - SOTTO cisterna comunale"
-plt.title(TITOLO) # modifica automatica a seconda del mese
-plt.ylim([0,425])
-plt.xlim([0,numGiorni+0.5])
-#plt.yticks(np.arange(0, 400, 20.0)) #modificare secondo cm altezza
-plt.xticks(np.arange(1, numGiorni+1, 1.0)) #modificare secondo numero a seconda dei giorni del mese
+fig.suptitle(TITOLO, fontsize=18, fontweight='bold')  # Make main title bold
 
-#SECONDO SUBPLOT
-plt.subplot(2, 1, 2)    #GRAFICO ACQUA COMUNALE
-plt.bar(giorniMeseIntJ, altezzeJ, width=0.5)
-addlabels(giorniMeseIntJ, altezzeJ)
-axes = plt.gca()
-axes.yaxis.grid()   #assi orizzontali
-plt.xlabel("GIORNO")
-plt.ylabel("ALTEZZA [cm]")
-#plt.title(TITOLO) # modifica automatica a seconda del mese
-plt.ylim([0,425])
-plt.xlim([0,numGiorni+0.5])
-#plt.yticks(np.arange(0, 400, 20.0)) #modificare secondo cm altezza
-plt.xticks(np.arange(1, numGiorni+1, 1.0)) #modificare secondo numero a seconda dei giorni del mese
-#plt.legend()
+# Style for the whole page
+plt.rcParams['axes.facecolor'] = '#f0f0f0' #change background color
+plt.rcParams['axes.edgecolor'] = '#000000' #add border to plot
 
-#fig = plots[0].figure  #sostituito da "fig = plt.figure()" vedi sopra
-#ax = plots[0].axis
-
-# mpld3.save_html(fig, "/var/www/html/acqua2.html") #salva solo se vado sul sito localhost ma gli mancano i permessi di root per salvare nella cartella
-plt_html = mpld3.fig_to_html(fig)
+plt_html = mpld3.fig_to_html(fig) #Generate html code
 Html_file = open("/home/pi/python-scripts/index.html", "w")
 Html_file.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">')
 Html_file.write(plt_html)
 print("File acqua.html creato!\n")
-#CARICAMENTO FILE SU GITHUB PAGES
-git_push()
 
+# Upload on Github Pages
+git_push()
